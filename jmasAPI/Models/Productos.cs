5d7f2d773471047693dc672e14bebfeb5b72b2b0
angelSantori <antonyo.da.mm@gmail.com>
2025-01-21 09:04:54 -0700

    Elemento repetido

--- jmasAPI/Models/Productos.cs
+++ jmasAPI/Models/Productos.cs
@@ -23,8 +23,6 @@ namespace jmasAPI.Models
 
         public string prodUMedEntrada { get; set; }
 
-        public double? prodPrecio { get; set; }
-
         public string prodImgB64 { get; set; }
 
         [ForeignKey("Proveedores")]
