3f553d5ec4493d020b0351bac16f61ac3ebb9348
angelSantori <antonyo.da.mm@gmail.com>
2025-01-23 09:33:43 -0700

    algo

--- jmasAPI/Models/Entradas.cs
+++ jmasAPI/Models/Entradas.cs
@@ -8,7 +8,7 @@ namespace jmasAPI.Models
         [Key]
         public int Id_Entradas { get; set; }
 
-        public string Entrada_CodFolio { get; set; }
+        public string? Entrada_CodFolio { get; set; }
 
         public string Entrada_Referencia { get; set; }
 
