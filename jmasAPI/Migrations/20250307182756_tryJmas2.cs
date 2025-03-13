9fff3fc8ef1ea7b6d2142d9ca8fd627377dcfd4f
angelSantori <antonyo.da.mm@gmail.com>
2025-03-12 21:55:58 -0700

    X -Migraciones

---
+++ jmasAPI/Migrations/20250307182756_tryJmas2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jmasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class tryJmas2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
