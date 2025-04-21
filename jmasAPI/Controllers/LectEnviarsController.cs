408a32a891d83e3ab20ef56796a996d7307816f9
angelSantori <antonyo.da.mm@gmail.com>
2025-04-20 20:07:30 -0700

    LectEnviar -Model -Controller

---
+++ jmasAPI/Controllers/LectEnviarsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using jmasAPI;
+using jmasAPI.Models;
+
+namespace jmasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LectEnviarsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LectEnviarsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LectEnviars
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LectEnviar>>> GetLectEnviar()
+        {
+            return await _context.LectEnviar.ToListAsync();
+        }
+
+        // GET: api/LectEnviars/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LectEnviar>> GetLectEnviar(int id)
+        {
+            var lectEnviar = await _context.LectEnviar.FindAsync(id);
+
+            if (lectEnviar == null)
+            {
+                return NotFound();
+            }
+
+            return lectEnviar;
+        }
+
+        // PUT: api/LectEnviars/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLectEnviar(int id, LectEnviar lectEnviar)
+        {
+            if (id != lectEnviar.idLectEnviar)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(lectEnviar).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LectEnviarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/LectEnviars
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<LectEnviar>> PostLectEnviar(LectEnviar lectEnviar)
+        {
+            _context.LectEnviar.Add(lectEnviar);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLectEnviar", new { id = lectEnviar.idLectEnviar }, lectEnviar);
+        }
+
+        // DELETE: api/LectEnviars/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLectEnviar(int id)
+        {
+            var lectEnviar = await _context.LectEnviar.FindAsync(id);
+            if (lectEnviar == null)
+            {
+                return NotFound();
+            }
+
+            _context.LectEnviar.Remove(lectEnviar);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LectEnviarExists(int id)
+        {
+            return _context.LectEnviar.Any(e => e.idLectEnviar == id);
+        }
+    }
+}
---
+++ jmasAPI/Models/LectEnviar.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jmasAPI.Models
+{
+    public class LectEnviar
+    {
+        [Key]
+        public int idLectEnviar { get; set; }
+
+        public string junta { get; set; }
+
+        public string lecturista { get; set; }                                
+
+        public string fecven { get; set; }
+
+        public string feccor { get; set; }
+
+        public int? cuenta { get; set; }
+
+        public string nombre { get; set; }
+
+        public string direccion { get; set; }
+
+        public string? colonia { get; set; } 
+
+        public int? contrato { get; set; }
+
+        public int? mesade { get; set; }
+
+        public string felean { get; set; }
+
+        [StringLength(5)]
+        public string servicio { get; set; }
+
+        [StringLength(5)]
+        public string tarifa { get; set; }        
+
+        public string? medidor { get; set; }
+
+        public int? lecant { get; set; }
+
+        public string? observ { get; set; }
+        
+        public byte lecact { get; set; }
+
+        public int? ruta { get; set; }        
+
+        public int? promedio { get; set; }        
+
+        [StringLength(5)]
+        public string leansn { get; set; }
+
+        [StringLength(5)]
+        public string zona { get; set; }               
+
+        public int idUsuario { get; set; }
+
+        public string img64 { get; set; }
+
+        public string? ubicacion { get; set; }
+
+        public int? conteo { get; set; }
+
+        public bool estado { get; set; }
+
+        [ForeignKey("ProblemasLectura")]
+        public int? idProblema { get; set; }
+    }
+}
