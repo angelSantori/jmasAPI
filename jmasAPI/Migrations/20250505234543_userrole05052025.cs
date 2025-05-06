2962bd3d9247de30fc7ebf2dd3b4470592c07ea8
angelSantori <antonyo.da.mm@gmail.com>
2025-05-05 23:13:55 -0700

    X -Migraciones

---
+++ jmasAPI/Migrations/20250505234543_userrole05052025.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jmasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class userrole05052025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "roleidRole",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_roleidRole",
+                table: "Users",
+                column: "roleidRole");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Role_roleidRole",
+                table: "Users",
+                column: "roleidRole",
+                principalTable: "Role",
+                principalColumn: "idRole");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Role_roleidRole",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_roleidRole",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "roleidRole",
+                table: "Users");
+        }
+    }
+}
---
+++ jmasAPI/Migrations/20250506004630_canAdd05052025.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace jmasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class canAdd05052025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "canAdd",
+                table: "Role",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "canAdd",
+                table: "Role");
+        }
+    }
+}
