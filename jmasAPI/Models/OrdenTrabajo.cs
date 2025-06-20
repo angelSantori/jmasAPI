85f4693d96bc2c17b855314b3930f0bcc1d2eff4
angelSantori <antonyo.da.mm@gmail.com>
2025-06-20 11:16:15 -0700

    Modelo OrdenTrabajo Controller OrdenTrabajo

---
+++ jmasAPI/Models/OrdenTrabajo.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jmasAPI.Models
+{
+    public class OrdenTrabajo
+    {
+        [Key]
+        public int idOrdenTrabajo {  get; set; }
+
+        public string folioOT { get; set; }
+
+        public string descripcionOT { get; set; }
+
+        public string fechaOT { get; set; }
+
+        public string medioOT { get; set; }
+
+        public bool materialOT { get; set; }
+
+        public string direccionOT { get; set; }
+
+        public string tipoProblemaOT { get; set; }
+
+        public string estadoOT { get; set; }
+
+        public string prioridadOT { get; set; }
+
+        [ForeignKey("Users")]
+        public int idUser { get; set; }
+
+        [ForeignKey("Padron")]
+        public int idPadron { get; set; }
+
+    }
+}
