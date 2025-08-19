801ca46e4919105da1b14202922f3739e6ff9192
angelSantori <antonyo.da.mm@gmail.com>
2025-08-18 21:17:12 -0700

    Asistencia Model

---
+++ jmasAPI/Models/Asistencia.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jmasAPI.Models
+{
+    public class Asistencia
+    {
+        [Key]
+        public int idAsistencia {  get; set; }
+        public string fechaAsistencia { get; set; }
+        public string horaAsistencia { get; set; }
+
+        [ForeignKey("Users")]
+        public int idUser { get; set; }
+    }
+}
