6724e6c51db0c8607a54db2c7a4c5da534fba74e
angelSantori <antonyo.da.mm@gmail.com>
2025-09-02 10:34:59 -0700

    Program cs - HttpClient

--- jmasAPI/Program.cs
+++ jmasAPI/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 // Habilitar CORS
 builder.Services.AddCors(options =>
