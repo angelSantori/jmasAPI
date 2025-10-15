90f59b80383eee892c74594e85ddca73b0f4b577
angelSantori <antonyo.da.mm@gmail.com>
2025-10-15 07:55:46 -0700

    ADD Contratistas Model

---
+++ jmasAPI/Models/Contratistas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jmasAPI.Models
+{
+    public class Contratistas
+    {
+        [Key]
+        public int idContratista { get; set; }
+        public string contratistaNombre { get; set; }
+        public string contratistaDireccion { get; set; }
+        public string contratistaTelefono { get; set; }
+        public string contratistaNumeroCuenta { get; set; }
+    }
+}
